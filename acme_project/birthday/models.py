e0ca236ecece2a89f7011575da4bc2e3282d0258
Roman Odinichenko <i@odirs.ru>
2024-04-18 17:50:26 +0300

    add validators

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField('Фамилия', blank=True, help_text='Необязательное поле', max_length=20)
-    birthday = models.DateField('День рождения')
+    birthday = models.DateField('День рождения', validators=(real_age,))
